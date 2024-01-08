bfabfe52226a043118a552c694dc90b9a45866af
Umar Igan <38042220+UmarIgan@users.noreply.github.com>
2024-01-08 17:35:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# .gitignore should include reference to config.py
+qdrant_url = "https://221fe4cd-a3f6-4aa5-82fd-c44068e97e54.eu-central-1-0.aws.cloud.qdrant.io:6333"
+qdrant_api_key = "Gx-MVrP94583g7WSJsd9ushZfLADgceh37HMUNhQX4-AbsS1kO0twg"
+
